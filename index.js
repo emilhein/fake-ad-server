6e32eac65f56febdf6a6791e2aa8a09d16ca4922
emilhein <emilhein1@gmail.com>
2023-03-08 13:29:15 +0100

    Remove cors package

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const express = require('express');
-let cors = require('cors');
 
 let cookieParser = require('cookie-parser');
 const app = express();
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: '*',
-  })
-);
+//Cors Configuration - Start
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested, Content-Type, Accept Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'POST, PUT, PATCH, GET, DELETE');
+    return res.status(200).json({});
+  }
+  next();
+});
+//Cors Configuration - End
 
 const appendClickArray = (req, clickedProduct) => {
   const cookies = req.cookies;
