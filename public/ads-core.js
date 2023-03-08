7f2f2db0191da17eef3561457d03a1389977e495
emilhein <emilhein1@gmail.com>
2023-03-08 21:41:01 +0100

    update static script

--- public/ads-core.js
+++ public/ads-core.js
@@ -1,1 +1,11 @@
 console.log('FROM THE CORE');
+const setCookie = (name, value, days) => {
+  var expires = '';
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = '; expires=' + date.toUTCString();
+  }
+  document.cookie = name + '=' + (value || '') + expires + '; path=/';
+};
+setCookie('youlike', 'somehing', 30);
