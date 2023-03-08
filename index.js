7bd8c16cbc7dcbcf635d24007b20472cb5f67c93
emilhein <emilhein1@gmail.com>
2023-03-08 14:48:25 +0100

    Use cors module

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 const express = require('express');
+const cors = require('cors');
 
 let cookieParser = require('cookie-parser');
 const app = express();
 app.use(cookieParser());
 //Cors Configuration - Start
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
-  );
-  next();
-});
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  })
+);
+// app.use(function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Credentials', true);
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
+//   );
+//   next();
+// });
 //Cors Configuration - End
 
 const appendClickArray = (req, clickedProduct) => {
