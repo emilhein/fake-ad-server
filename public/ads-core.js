c49e29a9aa4f0ef18282ed9828dd590b224ff92c
emilhein <emilhein1@gmail.com>
2023-03-09 14:27:15 +0100

    add helper function

--- public/ads-core.js
+++ public/ads-core.js
@@ -1,1 +1,12 @@
 console.log('I am the ads core script');
+
+globalThis.fakeAdserver = {
+  insertAdIntoDiv(id) {
+    const iframe = document.createElement('iframe');
+    iframe.style.display = 'none';
+    iframe.src = 'https://fake-ad-server.herokuapp.com/ad';
+    let element = document.getElementById(id);
+    element.appendChild(iframe);
+    console.log('Ad inserted');
+  },
+};
