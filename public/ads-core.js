69c78caa5aad9bab7741826288770121d0fd55c6
emilhein <emilhein1@gmail.com>
2023-03-09 14:58:26 +0100

    1 file modified

--- public/ads-core.js
+++ public/ads-core.js
@@ -2,15 +2,15 @@ console.log('I am the ads core script');
 
 globalThis.fakeAdserver = {
   insertAdIntoDiv(id) {
-    const src = fetch(`https://fake-ad-server.herokuapp.com/ad`).then((res) =>
-      res.text()
-    );
-
-    const iframe = document.createElement('iframe');
-    iframe.style.display = 'none';
-    iframe.src = src;
-    let element = document.getElementById(id);
-    element.appendChild(iframe);
-    console.log('Ad inserted');
+    fetch(`https://fake-ad-server.herokuapp.com/ad`)
+      .then((res) => res.text())
+      .then((url) => {
+        const iframe = document.createElement('iframe');
+        iframe.style.display = 'none';
+        iframe.src = url;
+        let element = document.getElementById(id);
+        element.appendChild(iframe);
+        console.log('Ad inserted');
+      });
   },
 };
