2f7083259d6dc0d0cd952c6ed753d8ab670d5053
emilhein <emilhein1@gmail.com>
2023-03-09 14:53:29 +0100

    get url dynamically

--- public/ads-core.js
+++ public/ads-core.js
@@ -2,9 +2,13 @@ console.log('I am the ads core script');
 
 globalThis.fakeAdserver = {
   insertAdIntoDiv(id) {
+    const src = fetch(`https://fake-ad-server.herokuapp.com/ad`).then((res) =>
+      res.text()
+    );
+
     const iframe = document.createElement('iframe');
     iframe.style.display = 'none';
-    iframe.src = 'https://fake-ad-server.herokuapp.com/ad';
+    iframe.src = src;
     let element = document.getElementById(id);
     element.appendChild(iframe);
     console.log('Ad inserted');
