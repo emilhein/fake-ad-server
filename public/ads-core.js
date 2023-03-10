2990ed16a661f3f3a72e354f9f22f800914287af
emilhein <emilhein1@gmail.com>
2023-03-10 11:21:46 +0100

    update creative displaying

--- public/ads-core.js
+++ public/ads-core.js
@@ -2,13 +2,17 @@ console.log('I am the ads core script');
 
 globalThis.fakeAdserver = {
   insertAdIntoDiv(id) {
-    fetch(`https://fake-ad-server.herokuapp.com/ad`,{
-      credentials: 'include' 
+    fetch(`https://fake-ad-server.herokuapp.com/ad`, {
+      credentials: 'include',
     })
       .then((res) => res.text())
       .then((path) => {
         const iframe = document.createElement('iframe');
-        iframe.style.display = 'none';
+        // iframe.style.display = 'none';
+        iframe.style.border = 'none';
+        iframe.style.height = '500px';
+        iframe.style.width = '500px';
+
         iframe.src = `https://fake-ad-server.herokuapp.com/${path}`;
         let element = document.getElementById(id);
         element.appendChild(iframe);
